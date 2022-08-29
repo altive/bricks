73e5af6ce8bf822f1ed13a1ccd6c6d1d24995475
Muramatsu Ryunosuke <muramatsu@altive.co.jp>
2022-08-30 07:48:19 +0900

    feat: add path variable

--- page/__brick__/lib/{{{path}}}/{{name.snakeCase()}}/{{name.snakeCase()}}_page.dart
+++ page/__brick__/lib/{{{path}}}/{{name.snakeCase()}}/{{name.snakeCase()}}_page.dart
@@ -4,8 +4,8 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'components/{{name.snakeCase()}}_page_app_bar.dart';
 import 'components/{{name.snakeCase()}}_page_body.dart';
 
-class {{name.pascalCase()}} extends HookConsumerWidget {
-  const {{name.pascalCase()}}({super.key});
+class {{name.pascalCase()}}Page extends HookConsumerWidget {
+  const {{name.pascalCase()}}Page({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
