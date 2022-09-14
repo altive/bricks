6090b44df721c128c0388bf07cf1ce2c4dea4525
Muramatsu Ryunosuke <muramatsu@altive.co.jp>
2022-09-14 17:17:35 +0900

    feat: change default paths or dependent packages

--- page/__brick__/lib/{{{path}}}/{{name.snakeCase()}}_page/components/{{name.snakeCase()}}_page_app_bar.dart
+++ page/__brick__/lib/{{{path}}}/{{name.snakeCase()}}_page/components/{{name.snakeCase()}}_page_app_bar.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:themes/themes.dart';
 
-import '../../../../util/localizer/localizer.dart';
-
 class {{name.pascalCase()}}PageAppBar extends StatelessWidget implements PreferredSizeWidget {
   const {{name.pascalCase()}}PageAppBar({super.key});
 
   @override
-  Size get preferredSize => appBarDefaultSize;
+  Size get preferredSize => AppBar().preferredSize;
 
   @override
   Widget build(BuildContext context) {
-    final l10n = L10n.of(context);
     return AppBar(
-      title: Text(l10n.{{name.camelCase()}}PageTitle),
+      title: Text('{{name.pascalCase()}}'),
     );
   }
 }
